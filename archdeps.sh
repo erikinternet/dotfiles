0c4475b8714f664d8d8029fb6c555abf26c225a3
erikinternet <erik.internetgE@tutanota.com>
2021-11-22 09:23:35 -0600

    ~/yay-bin

--- archdeps.sh
+++ archdeps.sh
@@ -5,5 +5,5 @@ sudo pacman -Sy pacman-contrib reflector rsync
 sudo reflector -a 10 -c us -f 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 #Applications
-git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin && cd yay-bin && makepkg -si
+git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin && cd ~/yay-bin && makepkg -si
 yay -Sy doas xorg-server xorg-xinit base-devel libx11 libxft libxinerama vim pacman-contrib reflector i3 fish rsync gzdoom dolphin-emu mupen64plus xwallpaper compton xterm
