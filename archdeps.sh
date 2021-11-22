bbb4e6e3af43f1af55ae0912a66a033c0438db79
erikinternet <erik.internetgE@tutanota.com>
2021-11-22 12:20:38 -0600

    added dt's surf

--- archdeps.sh
+++ archdeps.sh
@@ -3,7 +3,8 @@
 sudo pacman -Sy pacman-contrib reflector rsync
 #If you dont live in the us change it
 sudo reflector -a 10 -c us -f 5 --sort rate --save /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist
 
 #Applications
 git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin && cd ~/yay-bin && makepkg -si
-yay -Sy doas xorg-server xorg-xinit base-devel libx11 libxft libxinerama vim pacman-contrib reflector i3 fish rsync dolphin-emu mupen64plus xwallpaper compton xterm  acpi xorg-xrandr #gzdoom
+yay -Sy doas xorg-server xorg-xinit base-devel htop libx11 libxft libxinerama vim pacman-contrib reflector i3 fish keepassxc rsync dolphin-emu mupen64plus minizip xwallpaper compton xterm gst-libav gst-plugins-good gstreamer gcr webkit2gtk acpi xorg-xrandr #gzdoom
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 mkdir ~/repos && cd ~/repos
-mv ~/yay-bin ~/repos -r
+mv ~/yay-bin ~/repos
 cp ~/dotfiles ~/repos/ -r
 git clone https://github.com/erikinternet/st
 git clone https://github.com/erikinternet/pfetch
+git clone https://gitlab.com/dwt1/surf-distrotube
 git clone https://git.suckless.org/dmenu
 
 sudo vim /etc/doas.conf
 cd st && doas make clean install && doas cp 'Perfect DOS VGA 437.ttf' /usr/share/fonts/
 cd ../pfetch && doas make install 
 cd ../dmenu && doas make clean install
+cd ../surf-distrotube && doas make clean install
 
 # dotfiles
 cd ../dotfiles && pwd
