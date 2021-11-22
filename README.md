b7ef3c243143abcd77eebad107c2113391044c76
erikinternet <erik.internetgE@tutanota.com>
2021-11-21 22:30:51 -0600

    added doas setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles
 Only dep needed is git
+# Install
+Clone the repository, then cd into it, execute the dependency script and finally the setup.sh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 mkdir ~/repos && cd ~/repos
-git clone https://github.com/erikinternet/dotfiles
+cp ~/dotfiles ~/repos/
 git clone https://github.com/erikinternet/st
 git clone https://github.com/erikinternet/pfetch
 git clone https://git.suckless.org/dmenu
 
+sudo vim /etc/doas.conf
 cd st && doas make clean install && doas cp 'Perfect DOS VGA 437.ttf' /usr/share/fonts/
 cd ../pfetch && doas make install 
 cd ../dmenu && doas make clean install
